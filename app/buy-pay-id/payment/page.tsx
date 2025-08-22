ac5a25c7e28fd722b1b6a53c2dad653a40c1eec6
Jude-the-wizard☠️ <sjude9928@gmail.com>
2025-08-22 02:32:25 -0700

    Update page.tsx

--- app/buy-pay-id/payment/page.tsx
+++ app/buy-pay-id/payment/page.tsx
@@ -25,7 +25,7 @@ export default function PaymentPage() {
   }, [router])
 
   const handleCopyAmount = () => {
-    navigator.clipboard.writeText("6,100")
+    navigator.clipboard.writeText("7,250")
     setCopiedAmount(true)
     setTimeout(() => setCopiedAmount(false), 2000)
   }
@@ -69,7 +69,7 @@ export default function PaymentPage() {
             </div>
           </div>
           <div className="text-right">
-            <div className="text-xl font-bold">NGN 6,100</div>
+            <div className="text-xl font-bold">NGN 7,250</div>
             <div className="text-gray-600">{formData.email}</div>
           </div>
         </div>
@@ -81,7 +81,7 @@ export default function PaymentPage() {
             <div>
               <p className="text-gray-700 mb-1">Amount</p>
               <div className="flex items-center justify-between">
-                <p className="font-bold">NGN 6,100</p>
+                <p className="font-bold">NGN 7,250</p>
                 <button onClick={handleCopyAmount} className="bg-orange-400 text-white px-3 py-1 rounded text-sm">
                   {copiedAmount ? "Copied" : "Copy"}
                 </button>
@@ -91,7 +91,7 @@ export default function PaymentPage() {
             <div>
               <p className="text-gray-700 mb-1">Account Number</p>
               <div className="flex items-center justify-between">
-                <p className="font-bold">6936296932</p>
+                <p className="font-bold">5161493756</p>
                 <button
                   onClick={handleCopyAccountNumber}
                   className="bg-orange-400 text-white px-3 py-1 rounded text-sm"
@@ -103,12 +103,12 @@ export default function PaymentPage() {
 
             <div>
               <p className="text-gray-700 mb-1">Bank Name</p>
-              <p className="font-bold">Moniepoint MFB</p>
+              <p className="font-bold">Stanbic-ibtc bank</p>
             </div>
 
             <div>
               <p className="text-gray-700 mb-1">Account Name</p>
-              <p className="font-bold">Paygo-jude samuel</p>
+              <p className="font-bold">moneey-app jude samuel</p>
             </div>
           </div>
 
