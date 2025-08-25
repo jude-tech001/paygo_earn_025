e4e1dbfc0072a8cdb3cafe888c0db52dd6e24cde
Jude-the-wizard☠️ <sjude9928@gmail.com>
2025-08-25 11:48:09 +0100

    Update page.tsx

--- app/upgrade/payment/page.tsx
+++ app/upgrade/payment/page.tsx
@@ -32,7 +32,7 @@ export default function UpgradePaymentPage() {
   }
 
   const handleCopyAccountNumber = () => {
-    navigator.clipboard.writeText("5161493756")
+    navigator.clipboard.writeText("6936296932")
     setCopiedAccount(true)
     setTimeout(() => setCopiedAccount(false), 2000)
   }
@@ -95,7 +95,7 @@ export default function UpgradePaymentPage() {
                 <span>🔢</span> Account Number
               </p>
               <div className="flex items-center justify-between">
-                <p className="font-bold">5161493756</p>
+                <p className="font-bold">6936296932</p>
                 <button
                   onClick={handleCopyAccountNumber}
                   className="bg-orange-400 text-white px-3 py-1 rounded text-sm"
@@ -109,14 +109,14 @@ export default function UpgradePaymentPage() {
               <p className="text-gray-700 mb-1 text-sm flex items-center gap-1">
                 <span>🏦</span> Bank Name
               </p>
-              <p className="font-bold">Stanbic-ibtc bank</p>
+              <p className="font-bold">MONIEPIONT</p>
             </div>
 
             <div>
               <p className="text-gray-700 mb-1 text-sm flex items-center gap-1">
                 <span>🚹</span> Account Name
               </p>
-              <p className="font-bold">Moneeyapp-jude Samuel</p>
+              <p className="font-bold">Paygo-jude Samuel</p>
             </div>
           </div>
 
